e295c585470ac1f5789805345e636dc34b34f7c6
Rafael Paulo <rafaelpsrp@yahoo.com>
2025-05-13 21:33:48 -0300

    test: ensure DbAddAccount returns an account on sucess

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -107,4 +107,20 @@ describe('DbAddAccount', () => {
     const promise = sut.add(accountData)
     expect(promise).rejects.toThrow()
   })
+
+  test('Should call AddAccountRepository on sucess', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    })
+  })
 })
