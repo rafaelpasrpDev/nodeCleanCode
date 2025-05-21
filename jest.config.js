129e71436d2d4f1f064182917231d0d457fd0cb6
Rafael Paulo <rafaelpsrp@yahoo.com>
2025-05-21 00:26:29 -0300

    refactor: clean coverage archive in jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/presentation/protocols/index.ts',
+    '!<rootDir>/src/presentation/controller/signup/signup-protocols.ts',
+    '!<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts',
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
